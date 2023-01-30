6da9e407202dc6686854980b851b5d5acc1b4f7d
Kelvin <421321327@qq.com>
2023-01-30 22:58:49 +0800

    有状态组件 例子

--- lib/46浮动按钮 floatingActionButton.dart
+++ lib/46浮动按钮 floatingActionButton.dart
@@ -1,4 +1,4 @@
-// 有状态组件 例子
+// 浮动按钮
 import "package:flutter/material.dart";
 
 void main() {
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: "雷猴啊",
       theme: ThemeData(
-        primarySwatch: Colors.purple
+        primarySwatch: Colors.orange
       ),
       // home: HomePage(),
       home: const HomePage(),
@@ -35,30 +35,50 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
 
-  final List<String> _list = [];
+  int _numCount = 0;
+
 
   @override
   Widget build(BuildContext context) {
 
+    print("build时执行$_numCount");
+
     return Scaffold(
       // 加上 const 后，重新build时就不会修改静态数据，能提升一点性能
       appBar: AppBar(title: const Text("雷猴Flutter")),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "$_numCount",
+              style: Theme.of(context).textTheme.headline2
+            ),
+            const SizedBox(height: 60),
+            ElevatedButton(
+              onPressed: (){
+
+                // 使用 setState 后，会重新渲染页面
+                setState(() {
+                  _numCount--;
+                });
+                print("页面按钮 --");
+              },
+              child: const Text("减少")
+            )
+          ]
+        )
+      ),
+      // 浮动按钮
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          // 改变数据必须加上 setState
           setState(() {
-            _list.add("新增列表");
+            _numCount++;
+            print("浮动按钮 ++");
           });
         },
         child: const Icon(Icons.add)
       ),
-      body: ListView(
-        children: _list.map((v){
-          return ListTile(
-            title: Text(v)
-          );
-        }).toList(),
-      )
     );
   }
 }
